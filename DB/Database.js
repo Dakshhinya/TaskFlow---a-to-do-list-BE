755c984f1f2179936e1631772dffee2321bbf4d3
Dakshinya Loganathan <dakshinya@ajirasoft.com>
2025-10-14 10:59:01 +0530

    authMiddleware

---
+++ DB/Database.js
@@ -0,0 +1,19 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+const pool =({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    post : process.env.DB_PORT,
+})
+
+pool.connect()
+    .then(client=>{
+        console.log("Connected to database");
+        client.release();
+    })
+    .catch(err =>console.error("Error connecting to DB", err.stack))
+
+module.exports=pool;
