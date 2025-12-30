5134fda62bfa7d5a4125c9577be6c9ee2e8c839d
Dakshinya Loganathan <dakshinya@ajirasoft.com>
2025-12-30 12:27:46 +0530

    fixed server

--- server.js
+++ server.js
@@ -7,12 +7,22 @@ const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require("./routes/taskRoutes")
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: [
+    "http://localhost:5173",                 
+    "https://task-flow-frontend-lovat-seven.vercel.app"
+  ],
+  credentials:true
+}));
 
 app.use('/api/auth/',authRoutes);
 app.use('/api/task',taskRoutes);
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
 
-app.listen(3000,()=>{
-    console.log('Server listening to port 3000')
+const PORT =process.env.PORT||3000
+app.listen(PORT,()=>{
+    console.log(`Server listening to port ${PORT}`)
 })
