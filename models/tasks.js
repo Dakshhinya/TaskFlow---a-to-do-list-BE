c6907f6501da0ce5d03fb802017d701eef64c362
Dakshinya Loganathan <dakshinya@ajirasoft.com>
2026-01-01 23:12:03 +0530

    fixed the update in fe not in be

--- models/tasks.js
+++ models/tasks.js
@@ -2,7 +2,7 @@ const pool = require('../DB/Database')
 
 const createTask = async ({ userId, taskname, priority, start_date, start_time, end_date, end_time, status }) =>{
     const result = await pool.query(
-        `INSERT INTO tasks (userId, taskname, priority, start_date, start_time, end_date, end_time, status) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *`,
+        `INSERT INTO tasks (userId, taskname, priority, start_date, start_time, end_date, end_time, status) VALUES($1,$2,$3,$4,$5,$6,$7, $8) RETURNING *`,
         [userId, taskname, priority, start_date, start_time, end_date, end_time, status]
     );
     return result.rows[0];
@@ -15,5 +15,5 @@ const displayTask = async (userId)=>{
     )
     return result.rows;
 }
-
+// const updateTask = async({userId, taskname,})
 module.exports ={createTask, displayTask}
