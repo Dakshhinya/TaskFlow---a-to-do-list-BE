4cf89333885f96789a26b06e917ba33c70c57ebd
Dakshinya Loganathan <dakshinya@ajirasoft.com>
2026-01-11 01:43:01 +0530

    changed signIn

--- models/login.js
+++ models/login.js
@@ -1,8 +1,10 @@
 const pool = require('../DB/Database')
 
 const checkUser = async ({email, password})=>{
-    const result = await pool.query('SELECT * FROM users WHERE email=$1 AND password=$2',([email, password]));
-    return result.rows[0];
+const result = await pool.query(
+    'SELECT * FROM users WHERE email = $1',
+    [email]);
+        return result.rows[0];
 }
 
 module.exports = checkUser;
